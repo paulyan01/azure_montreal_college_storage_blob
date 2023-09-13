d8ae691154f75d30c2a18c8a8a1c1cd494fc64da
paulyan01 <126923309+paulyan01@users.noreply.github.com>
2023-09-13 18:25:31 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,3 +3,15 @@ resource "azurerm_resource_group" "azureresourcegroup" {
   name     = "MCIT_resource_group"
   location = "Canada Central"
 }
+resource "azurerm_storage_account" "example" {
+  name                     = "MCIT-azurerm_strg_acc"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+resource "azurerm_storage_container" "example" {
+  name                  = "MCIT_azrm_container"
+  storage_account_name  = azurerm_storage_account.example.name
+  container_access_type = "private"
+}
