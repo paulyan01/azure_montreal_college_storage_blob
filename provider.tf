d0bb861c17ae0f1235e1fc26376177ddd2d3f003
paulyan01 <126923309+paulyan01@users.noreply.github.com>
2023-09-13 18:44:46 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -4,10 +4,13 @@ terraform{
       source="hashicorp/azurerm"
       version=">=3.59.0"#This is the version of Azure Rm module
     }
+}
     required_version=">=1.4.0"  #This is the version of Terraform 
-  }
 }
 provider "azurerm" {
+
+    features{}
+    skip_provider_registration=true
     client_id=var.client_id
     client_secret=var.client_secret
     subscription_id=var.subscription_id
