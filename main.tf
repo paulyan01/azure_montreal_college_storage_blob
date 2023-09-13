86659a6c68fed4617bdec48d959a7fb96720b4e1
Sami Islam <islam.samipkr@gmail.com>
2023-09-12 20:40:16 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 #This is an Azure Montreal College Tutorial for Storage Account creation--->Storage Container name Creation--->Storage Blob Creation
 resource "azurerm_resource_group" "azureresourcegroup" {
   name     = "MCIT_resource_group"
-  location = "canadacentral"
+  location = "Canada Central"
 }
