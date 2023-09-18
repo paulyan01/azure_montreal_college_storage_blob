f74c8f638a6aca83696ea244c48b2b0f402536f1
paulyan01 <126923309+paulyan01@users.noreply.github.com>
2023-09-18 19:57:41 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,3 +22,34 @@ resource "azurerm_storage_blob" "azurestorageblob" {
   type                   = "Block"
   source                 = "some-local-file.zip"
 }
+resource "azurerm_kubernetes_cluster" "k8scluster" {
+  name                = "${var.prefix}cluster"
+  location            = azurerm_resource_group.azureresourcegroup.location
+  resource_group_name = azurerm_resource_group.azureresourcegroup.name
+  dns_prefix          = "exampleaks1"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Production"
+  }
+}
+
+output "client_certificate" {
+  value     = azurerm_kubernetes_cluster.k8scluster.kube_config.0.client_certificate
+  sensitive = true
+}
+
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.k8scluster.kube_config_raw
+
+  sensitive = true
+}
