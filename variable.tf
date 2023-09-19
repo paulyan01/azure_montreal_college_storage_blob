e2e53c57f38f8d9f2a15c6d46cb03558351092ff
paulyan01 <126923309+paulyan01@users.noreply.github.com>
2023-09-18 20:21:23 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -21,3 +21,12 @@ variable "address_space"{
   type=list(string)
   default=["10.0.0.0/16","10.0.1.0/24"]
 }
+variable "computer_name"{
+  type=string
+}
+variable "admin_username"{
+  type=string
+}
+variable "admin_password"{
+  type=string
+}
