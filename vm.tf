62d70b4c7f07a9b8c3dbadaa262d5a08bf8749fc
paulyan01 <126923309+paulyan01@users.noreply.github.com>
2023-09-18 20:08:02 -0400

    Update vm.tf

--- vm.tf
+++ vm.tf
@@ -1,21 +1,21 @@
 resource "azurerm_virtual_network" "mcitvmnet" {
   name                = "${var.prefix}-network"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.azureresourcegroup.location
+  resource_group_name = azurerm_resource_group.azureresourcegroup.name
 }
 
 resource "azurerm_subnet" "internal" {
   name                 = "internal"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.main.name
+  resource_group_name  = azurerm_resource_group.azureresourcegroup.name
+  virtual_network_name = azurerm_virtual_network.mcitvmnet.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
 resource "azurerm_network_interface" "main" {
   name                = "${var.prefix}-nic"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.azureresourcegroup.location
+  resource_group_name = azurerm_resource_group.azureresourcegroup.name
 
   ip_configuration {
     name                          = "testconfiguration1"
@@ -26,8 +26,8 @@ resource "azurerm_network_interface" "main" {
 
 resource "azurerm_virtual_machine" "main" {
   name                  = "${var.prefix}-vm"
-  location              = azurerm_resource_group.example.location
-  resource_group_name   = azurerm_resource_group.example.name
+  location              = azurerm_resource_group.azureresourcegroup.location
+  resource_group_name   = azurerm_resource_group.azureresourcegroup.name
   network_interface_ids = [azurerm_network_interface.main.id]
   vm_size               = "Standard_DS1_v2"
 
