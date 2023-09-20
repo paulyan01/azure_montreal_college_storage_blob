c61167bffb43bb958fbd076adb5bd15f3d2b72ad
paulyan01 <126923309+paulyan01@users.noreply.github.com>
2023-09-20 19:25:20 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -57,3 +57,25 @@ output "kube_config" {
 
   sensitive = true
 }
+resource "azurerm_sql_server" "mcitsqlserv" {
+  name                         = "mcitsqlserver"
+  resource_group_name          = azurerm_resource_group.azureresourcegroup.name
+  location                     = azurerm_resource_group.azureresourcegroup.location
+  version                      = "12.0"
+  administrator_login          = "4dm1n157r470r"
+  administrator_login_password = "4-v3ry-53cr37-p455w0rd"
+
+  tags = {
+    environment = "production"
+  }
+}
+resource "azurerm_sql_database" "mcitsqldb" {
+  name                = "mcitsqldatabase"
+  resource_group_name = azurerm_resource_group.azureresourcegroup.name
+  location            = azurerm_resource_group.azureresourcegroup.location
+  server_name         = azurerm_sql_server.mcitsqlserv.name
+
+  tags = {
+    environment = "production"
+  }
+}
