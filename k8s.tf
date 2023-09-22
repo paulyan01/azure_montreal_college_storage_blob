3ea9b4952814c04a0165363b5e6529e6ac5d3f03
paulyan01 <126923309+paulyan01@users.noreply.github.com>
2023-09-22 19:40:09 -0400

    Create k8s.tf

---
+++ k8s.tf
@@ -0,0 +1,33 @@
+resource "azurerm_kubernetes_cluster" "k8scluster" {
+  #for_each            ={for cluster in local.cluster_names:cluster=>cluster}
+  name                = "${var.prefix}cluster"
+  #-${each.key}
+  location            = azurerm_resource_group.azureresourcegroup.location
+  resource_group_name = azurerm_resource_group.azureresourcegroup.name
+  dns_prefix          = "exampleaks1"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Production"
+  }
+}
+
+output "client_certificate" {
+  value     = azurerm_kubernetes_cluster.k8scluster.kube_config.0.client_certificate
+  sensitive = true
+}
+
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.k8scluster.kube_config_raw
+
+  sensitive = true
+}
