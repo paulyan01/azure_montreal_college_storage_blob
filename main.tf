7b62694c1ff8445c80eee7213e4a7d0d0bbaf907
paulyan01 <126923309+paulyan01@users.noreply.github.com>
2023-09-22 19:09:47 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 #This is an Azure Montreal College Tutorial for Storage Account creation--->Storage Container name Creation--->Storage Blob Creation
-locals{ 
-  cluster_names=["mcitk8s","mcitk8s2","mcitk8s3","mcitk8s4"]
-}
+#locals{ 
+#  cluster_names=["mcitk8s","mcitk8s2","mcitk8s3","mcitk8s4"]
+#}
 resource "azurerm_resource_group" "azureresourcegroup" {
   name     = "MCIT_resource_group"
   location = "Canada Central"
