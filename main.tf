fbfe76afa9f7a892e7825bd9830fa4ce25ea1de0
paulyan01 <126923309+paulyan01@users.noreply.github.com>
2023-09-22 18:52:03 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -62,20 +62,21 @@ resource "azurerm_mssql_server" "mcitsqlserv" {
   resource_group_name          = azurerm_resource_group.azureresourcegroup.name
   location                     = azurerm_resource_group.azureresourcegroup.location
   version                      = "12.0"
-  administrator_login          = "4dm1n157r470r"
-  administrator_login_password = "4-v3ry-53cr37-p455w0rd"
-
-  tags = {
-    environment = "production"
-  }
+  administrator_login          = var.administrator_login
+  administrator_login_password = var.administrator_login_password
 }
-resource "azurerm_mssql_database" "mcitsqldb" {
-  name                = "mcitsqldatabase"
-  resource_group_name = azurerm_resource_group.azureresourcegroup.name
-  location            = azurerm_resource_group.azureresourcegroup.location
-  server_name         = azurerm_sql_server.mcitsqlserv.name
+
+resource "azurerm_mssql_database" "test" {
+  name           = "acctest-db-d"
+  server_id      = azurerm_mssql_server.mcitsqlserv.id
+  collation      = "SQL_Latin1_General_CP1_CI_AS"
+  license_type   = "LicenseIncluded"
+  max_size_gb    = 4
+  read_scale     = true
+  sku_name       = "S0"
+  zone_redundant = true
 
   tags = {
-    environment = "production"
+    foo = "bar"
   }
 }
