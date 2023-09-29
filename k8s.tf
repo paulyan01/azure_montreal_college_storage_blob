10b5cb2ccda0d372936c3451248d665a5daf682b
paulyan01 <126923309+paulyan01@users.noreply.github.com>
2023-09-29 18:26:47 -0400

    Update k8s.tf

--- k8s.tf
+++ k8s.tf
@@ -30,3 +30,19 @@ output "kube_config" {
 
   sensitive = true
 }
+service_principal {
+    client_id     = var.client_id
+    client_secret = var.cliebt_secret
+  }
+}
+resource "azurerm_kubernetes_cluster_node_pool" "clusterpool" {
+  for_each              = {for pool in local.pool_name: pool=>pool}
+  name                  = "internal"
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.k8scluster.id
+  vm_size               = "Standard_DS2_v2"
+  node_count            = 1
+
+  tags = {
+    Environment = "Production"
+  }
+}
