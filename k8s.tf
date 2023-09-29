111c25c9bc748245f24abf7d3f2d49650ebfa38e
paulyan01 <126923309+paulyan01@users.noreply.github.com>
2023-09-29 18:40:27 -0400

    Update k8s.tf

--- k8s.tf
+++ k8s.tf
@@ -37,7 +37,7 @@ output "kube_config" {
 resource "azurerm_kubernetes_cluster_node_pool" "clusterpool" {
   for_each              = {for pool in local.pool_name: pool=>pool}
   name                  = "internal-${each.key}"
-  kubernetes_cluster_id = azurerm_kubernetes_cluster.k8scluster: cluster.id
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.k8scluster: pool.id
   vm_size               = "Standard_DS2_v2"
   node_count            = 1
 
