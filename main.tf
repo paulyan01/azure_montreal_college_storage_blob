741e84eb73e15c74022662893f9ea8465f4e51f4
paulyan01 <126923309+paulyan01@users.noreply.github.com>
2023-10-02 18:24:22 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 locals{ 
   storage_name= ["north","south","east","west"]
 #  clusters_name= ["paul","saj","jade","douglas","emannuel","olarewaju","oladipupo"]
-  cluster_name= ["earth","wind","fire","water","air"]
+  clusters_name= ["earth","wind","fire","water","air"]
   appgt_name= ["batman","robin","redhood","nightwing","batgirl"]
   vm_name= ["bread","lettuce","tomato","bacon","mayonaise"]
 #  cluster_names=["mcitk8s","mcitk8s2","mcitk8s3","mcitk8s4"]
