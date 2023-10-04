cbd3e4d8358e6b92ddea65d1840a3796150ccb55
paulyan01 <126923309+paulyan01@users.noreply.github.com>
2023-10-04 18:59:14 -0400

    Update linwebapp.tf

--- linwebapp.tf
+++ linwebapp.tf
@@ -1,20 +1,3 @@
-/*resource "azurerm_service_plan" "azserplan" {
-  name                = "${var.prefix}-azserplan"
-  resource_group_name = azurerm_resource_group.azureresourcegroup.name
-  location            = azurerm_resource_group.azureresourcegroup.location
-  os_type             = "Linux"
-  sku_name            = "P1v2"
-}
-
-resource "azurerm_linux_web_app" "linwebapp" {
-  for_each            = {for linux in local.linux_name: linux=>linux}
-  name                = "${var.prefix}-linwebapp-${each.key}"
-  resource_group_name = azurerm_resource_group.azureresourcegroup.name
-  location            = azurerm_service_plan.azserplan.location
-  service_plan_id     = azurerm_service_plan.azserplan.id
-
-  site_config {}
-}
 resource "azurerm_service_plan" "azserplan" {
   for_each            ={for sp in local.linux_app_list: "$sp.name"=>sp }
   name                = each.value.name
@@ -30,6 +13,23 @@ resource "azurerm_linux_web_app" "batcha06webapp" {
   resource_group_name = azurerm_resource_group.azureresourcegroup.name
   location            = azurerm_resource_group.azureresourcegroup.location
   service_plan_id     = each.value.id
+
+  site_config {}
+}
+/*resource "azurerm_service_plan" "azserplan" {
+  name                = "${var.prefix}-azserplan"
+  resource_group_name = azurerm_resource_group.azureresourcegroup.name
+  location            = azurerm_resource_group.azureresourcegroup.location
+  os_type             = "Linux"
+  sku_name            = "P1v2"
+}
+
+resource "azurerm_linux_web_app" "linwebapp" {
+  for_each            = {for linux in local.linux_name: linux=>linux}
+  name                = "${var.prefix}-linwebapp-${each.key}"
+  resource_group_name = azurerm_resource_group.azureresourcegroup.name
+  location            = azurerm_service_plan.azserplan.location
+  service_plan_id     = azurerm_service_plan.azserplan.id
 
   site_config {}
 }
