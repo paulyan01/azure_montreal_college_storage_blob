6fa24d9f41043f9d51428bf7fe3bc48f9e55d8fc
paulyan01 <126923309+paulyan01@users.noreply.github.com>
2023-10-04 18:57:36 -0400

    Update linwebapp.tf

--- linwebapp.tf
+++ linwebapp.tf
@@ -15,3 +15,21 @@ resource "azurerm_linux_web_app" "linwebapp" {
 
   site_config {}
 }/*
+resource "azurerm_service_plan" "azserplan" {
+  for_each            ={for sp in local.linux_app_list: "$sp.name"=>sp }
+  name                = each.value.name
+  resource_group_name = azurerm_resource_group.azureresourcegroup.name
+  location            = azurerm_resource_group.azureresourcegroup.location
+  os_type             = each.value.os_type
+  sku_name            = each.value.sku_name
+}
+
+resource "azurerm_linux_web_app" "batcha06webapp" {
+  for_each            = azurerm_service_plan.azserplan
+  name                = each.value.name
+  resource_group_name = azurerm_resource_group.azureresourcegroup.name
+  location            = azurerm_resource_group.azureresourcegroup.location
+  service_plan_id     = each.value.id
+
+  site_config {}
+}
