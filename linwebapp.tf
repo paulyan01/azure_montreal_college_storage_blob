76afcbb80be211cfccb94809ff7d20ed0c51fbd4
paulyan01 <126923309+paulyan01@users.noreply.github.com>
2023-10-04 20:30:21 -0400

    Update linwebapp.tf

--- linwebapp.tf
+++ linwebapp.tf
@@ -1,24 +1,9 @@
-locals{
-  linux_app=[for f in fileset("${path.module}/configs", "[^_]*.yaml") : yamldecode(file("${path.module}/configs/${f}"))]
-  linux_app_list = flatten([
-    for app in local.linux_app : [
-      for linuxapps in try(app.linux_app, []) :{
-        name=linuxapps.name
-        resource_group_name=linuxapps.resource_group
-        location=linuxapps.location
-        os_type=linuxapps.os_type
-        sku_name=linuxapps.sku_name     
-      }
-    ]
-])
-}
 resource "azurerm_resource_group" "azureresourcegroup" {
 for_each={for rg in local.linux_app_list:"${rg.name} => rg}  
   name     = each.value.name
   location = each.value.location
 }
-/*
-resource "azurerm_service_plan" "azserplan" {
+/*resource "azurerm_service_plan" "azserplan" {
   for_each            ={for sp in local.linux_app_list: "$sp.name"=>sp }
   name                = each.value.name
   resource_group_name = azurerm_resource_group.azureresourcegroup.name
@@ -53,4 +38,3 @@ resource "azurerm_linux_web_app" "linwebapp" {
 
   site_config {}
 }
-/*
